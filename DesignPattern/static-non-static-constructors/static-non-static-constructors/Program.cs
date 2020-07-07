320f2c844b2f7e70c00bc8f807b93d0813b1db10
gitName <a@b.c>
2020-07-07 09:39:35 -0400

    Difference between Static Constructors and Non-Static Constructors

---
+++ DesignPattern/static-non-static-constructors/static-non-static-constructors/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace static_non_static_constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Difference between Static Constructors and Non-Static Constructors*/
+            //Greek greek = new Greek();
+            //Greek _greek = new Greek();
+
+
+            //Method Overloading
+            Ploytest ploytest = new Ploytest();
+            Console.WriteLine("Output" + ploytest.add(54564564, 1));
+            Console.ReadLine();
+        
+
+        }
+    }
+}
+public class Ploytest
+{
+    public int add(int a,int b)
+    {
+        return a + b;
+    }
+
+    public long add(long a, long b)
+    {
+        return a + b;
+    }
+}
+
+public class Greek
+{
+    static Greek()
+    {
+        Console.WriteLine("I am static constructor");
+    }
+
+    public Greek()
+    {
+        Console.WriteLine("I am Non-Static constructor");
+    }
+
+}
